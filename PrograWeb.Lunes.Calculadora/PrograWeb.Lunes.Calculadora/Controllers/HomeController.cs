8581a4bb699109bd04cd18adb53478bf34dd7490
Laboratorios <Laboratorios@R25>
2018-09-24 20:46:45 -0600

    proyecto de Lógica de Negocio

--- PrograWeb.Lunes.Calculadora/PrograWeb.Lunes.Calculadora/Controllers/HomeController.cs
+++ PrograWeb.Lunes.Calculadora/PrograWeb.Lunes.Calculadora/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace PrograWeb.Lunes.Calculadora.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page";
 
             return View();
         }
